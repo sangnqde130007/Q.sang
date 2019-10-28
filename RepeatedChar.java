42e439e3551580626569a28bc420b4ddd8629990
sangnqde130007 <57082592+sangnqde130007@users.noreply.github.com>
2019-10-28 12:26:01 +0700

    Add files via upload

---
+++ RepeatedChar.java
@@ -0,0 +1,44 @@
+package exercise6;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class RepeatedChar {
+ public static String repeatfunc(String s) {
+        String[] words = s.split("\\s");
+        ArrayList<String> words2 = new ArrayList<>();
+        String result = "";
+        int k = 0;
+        for (String w : words) {
+            words2.add(w);
+        }
+        ArrayList<Integer> index = new ArrayList<>();
+        for (int i = 0; i < words2.size(); i++) {
+            for (int j = i + 1; j < words2.size(); j++) {
+                if (words2.get(i).equals(words2.get(j))) { 
+                    index.add(j);
+                    words2.set(i,"REPEAT");
+                }   
+                else{
+                    break;
+                }
+            } 
+        }
+        for (Integer integer : index) {
+            words2.set(integer, "");
+        }
+        for (String string : words2) {
+            if(!string.equals("")){
+                result += string+" ";
+            }
+        }
+        result=result.replace("\\s+","");
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(repeatfunc("I want to to go to the the the zoo"));
+    }
+
+}
